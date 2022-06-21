53e0efa6005b17476b17cc26e871dcd61c9d3cd7
Cinthia M. Orona <devrider2021@gmail.com>
2022-06-21 15:57:53 -0300

    Listo el segundo commit, pasé función suma al main.

--- main.py
+++ main.py
@@ -3,6 +3,12 @@
 # Date: 21/06/22
 # Description: Repositorio de prácticas para la materia Programación del ISPC 1° Año.
 
+def suma(a,b,c):
+    return(a+b+c)
+
+
+
 
 if __name__=="__main__":
     print("Primer programa en Python !!")
+    print("La suma es: ", str(suma(1,2,3)))
--- suma.py
+++
@@ -1,5 +0,0 @@
-def suma(a,b,c):
-    resultado = a+b+c
-    return resultado
-
-print(suma(1,2,3))
