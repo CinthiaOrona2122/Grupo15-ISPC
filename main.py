75ec9b726a84580b932e9a76c813b86d2fb966bb
Tomas Lencina <91835892+TomiLencina@users.noreply.github.com>
2022-06-24 12:43:22 -0300

    Corregí la resta
    Incorpore los input

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ def suma(a,b,c):
     return(a+b+c)
 
 def resta(a,b):
+    a = int(input("Ingrese el primer numero: "))
+    b = int(input("Ingrese el segundo numero: "))    
     return(a-b)
 
 
 if __name__=="__main__":
     print("Primer programa en Python !!")
     print("La suma es: ", str(suma(1,2,3)))
-    print("La resta es: ", str(resta(5,3)))
+    print("La resta es: ", str(resta))
